9fd44d09af5abc1c7e630310fea22529fe674b4f
wenbiaoliu <bill.wl@163.com>
2014-07-18 09:35:41 +0800

    add pepole class

--- vulcan.lua
+++ vulcan.lua
@@ -1,3 +1,19 @@
 function vulcan()
 	print('new feature')
 end
+
+Pepole = class()
+
+function Pepole:ctor(name)
+	self.name = name
+	self.email = ''
+end
+
+function Pepole:GetName()
+	return self.name
+end
+
+function Pepole:GetEmail()
+	return self.email
+end
+
